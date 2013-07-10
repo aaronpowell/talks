a193177abf34492656b8513a9f1dfa2b8b602174
Aaron Powell <me@aaron-powell.com>
2013-07-11 09:17:16 +1000

    starting thunderscore and enumerable implementation

--- javascript-all-the-things/_code/thunderscore.js
+++ javascript-all-the-things/_code/thunderscore.js
@@ -1,4 +1,23 @@
 (function (global) {
     'use strict';
     global._ = {};
+
+    var zip = function (array) {
+        var args = arguments.length == 1 ? arguments[0] : Array.prototype.slice.call(arguments, 0);
+
+        var len = args.map(function (x) { return x.length; }).sort()[0];
+
+        var result = new Array(len);
+
+        var index = -1;
+
+        while (++index < len) {
+        result[index] = args.map(function (x) { return x[index]; });
+        }
+
+        return result;
+    };
+
+    global._.zip = zip;
+    global._.unzip = zip;
 })(window);
