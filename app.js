f5df61be1974cf959a6171a0c7c3c40fcdf6ed38
Aaron Powell <me@aaron-powell.com>
2013-08-26 18:58:27 -0500

    implemeting basic AJAX example

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
 app.use(express.static(__dirname));
+
+var flightModeRoutes = require('./flight-mode/routes.js');
+
+app.post('/flight-mode', flightModeRoutes['flight-mode']);
 
 app.listen(process.env.PORT || 3000);
